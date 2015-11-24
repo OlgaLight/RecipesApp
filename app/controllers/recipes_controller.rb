00ae6f63e0991baac365776c0de9cbc13224a9bd
OlgaLight <marshak.oggy@gmail.com>
2015-11-24 11:14:31 +0200

    add image uploader

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -36,13 +36,12 @@ class RecipesController < ApplicationController
     else
       render :edit
     end
-
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :summary, :description)
+    params.require(:recipe).permit(:name, :summary, :description, :picture)
   end
 
 end
